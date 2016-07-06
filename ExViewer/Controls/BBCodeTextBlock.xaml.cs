6c388706e1dea8198f856e49398c7ab22754eff6
OpportunityLiu <liuzhengyuan13@outlook.com>
2016-07-06 20:18:13 +0800

    UI for torrents and comments
    Former-commit-id: 3af4386b98ff5b7277e34797c4a240388ed88e95 [formerly e79218d253986d8a09b735f5b7501015497048cf]
    Former-commit-id: 92e73d5849686c8d851909e39e3dd834aec73448

---
+++ ExViewer/Controls/BBCodeTextBlock.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ExViewer.Controls
+{
+    public sealed partial class BBCodeTextBlock : UserControl
+    {
+        public BBCodeTextBlock()
+        {
+            this.InitializeComponent();
+        }
+
+        public Style TextBlockStyle
+        {
+            get
+            {
+                return (Style)GetValue(TextBlockStyleProperty);
+            }
+            set
+            {
+                SetValue(TextBlockStyleProperty, value);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for TextBlockStyle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextBlockStyleProperty =
+            DependencyProperty.Register("TextBlockStyle", typeof(Style), typeof(BBCodeTextBlock), new PropertyMetadata(null));
+
+        public string BBCode
+        {
+            get
+            {
+                return (string)GetValue(BBCodeProperty);
+            }
+            set
+            {
+                SetValue(BBCodeProperty, value);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for BBCode.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BBCodeProperty =
+            DependencyProperty.Register("BBCode", typeof(string), typeof(BBCodeTextBlock), new PropertyMetadata("", BBCodePropertyChanged));
+
+        public static void BBCodePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(e.NewValue == null)
+                throw new ArgumentNullException(nameof(BBCode));
+            ((BBCodeTextBlock)sender).loadBBCode(e.NewValue.ToString());
+        }
+
+        private void loadBBCode(string bbCode)
+        {
+            Content.Text = bbCode;
+        }
+    }
+}
