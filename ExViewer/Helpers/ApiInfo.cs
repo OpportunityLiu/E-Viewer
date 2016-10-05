0b353d920187af3b6d615354b6533849a8c16e66
OpportunityLiu <liuzhengyuan13@outlook.com>
2016-10-05 21:46:36 +0800

    api info platform-specific attribute

--- ExViewer/Helpers/ApiInfo.cs
+++ ExViewer/Helpers/ApiInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using static Windows.Foundation.Metadata.ApiInformation;
@@ -9,16 +10,19 @@ namespace ExViewer
 {
     public static class ApiInfo
     {
+        [PlatformSpecific]
         public static bool CommandBarDynamicOverflowSupported
         {
             get;
         } = IsPropertyPresent("Windows.UI.Xaml.Controls.CommandBar", "IsDynamicOverflowEnabled");
 
+        [PlatformSpecific]
         public static bool StatusBarSupported
         {
             get;
         } = IsTypePresent("Windows.UI.ViewManagement.StatusBar");
 
+        [PlatformSpecific]
         public static bool AnimatedGifSupported
         {
             get;
