767f97eee906bdcf43deade3745ab63ebfb4e42b
OpportunityLiu <liuzhengyuan13@outlook.com>
2016-11-03 17:31:35 +0800

    japanese in time
    Former-commit-id: d1831ba0288280f5dd3ecae617b2c130dbe4f2d0 [formerly a4ce21889513292f5e666de2b14ac65c76f35739]
    Former-commit-id: 99a9a237a969cdc8704a101131d19b43db259e9b

---
+++ ExViewer/Helpers/Box{T}.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Threading;
+
+namespace ExViewer
+{
+    public class Box<T> : INotifyPropertyChanged
+    {
+        public Box(T value)
+        {
+            this.value = value;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName]string name = null)
+        {
+            var temp = PropertyChanged;
+            if(temp == null)
+                return;
+            DispatcherHelper.CheckBeginInvokeOnUI(() => temp(this, new PropertyChangedEventArgs(name)));
+        }
+
+        private T value;
+
+        public T Value
+        {
+            get
+            {
+                return value;
+            }
+            set
+            {
+                this.value = value;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
