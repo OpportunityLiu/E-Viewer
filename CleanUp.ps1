efc3c50b1444cfecebf5609292f5508e07c926fb
OpportunityLiu <liuzhengyuan13@outlook.com>
2016-11-05 19:26:59 +0800

    Workaround for .net clrcompression.dll bug
    Former-commit-id: 5412475dac241ea550e1f967567f9b118924c1c0 [formerly 89d6f4ce842ba3d836a2f90fa94707dda1d492d0]
    Former-commit-id: a66d6b26bb1a7d2ffbfca6e9434de94a2228afa6

--- CleanUp.ps1
+++ CleanUp.ps1
@@ -1,8 +1,15 @@
-$folders = Get-ChildItem -Directory -Recurse -Depth 1;
+$folders = Get-ChildItem -Recurse -Depth 1;
 foreach($item in $folders)
 {
-    if($item.Name -eq "obj" -or $item.Name -eq "bin")
+    if($item.Name -eq "obj" -or $item.Name -eq "bin" -or $item.Name -eq "project.lock.json")
     {
-        $item.Delete($true);
+        if($item.PSIsContainer)
+        {
+            $item.Delete($true);
+        }
+        else
+        {
+            $item.Delete();
+        }
     }
 }
