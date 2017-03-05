362b554286b48c75e64997a667677da73d67779d
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-03-05 15:45:16 +0800

    new version
    Former-commit-id: 47668002fcda2529cb461103139545cbe79c5fff [formerly 085397cd1d3e134a03518f2dc1ea51767c1f55e5]
    Former-commit-id: abb18c5109b7f3ff67c8ff9a82016394ff5e6f57

--- CleanUp.ps1
+++ CleanUp.ps1
@@ -1,7 +1,7 @@
 $folders = Get-ChildItem -Recurse -Depth 1;
 foreach($item in $folders)
 {
-    if($item.Name -eq "obj" -or $item.Name -eq "bin" -or $item.Name -eq "project.lock.json")
+    if($item.Name -eq "obj" -or $item.Name -eq "bin" -or $item.Name -eq "project.lock.json" -or $item.Name -like "*.nuget.*")
     {
         if($item.PSIsContainer)
         {
