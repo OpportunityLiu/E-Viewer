a26bdf3dfbe45f7f7e68c1ace29b3e516b1445f2
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-04-09 17:39:00 +0800

    update to 15063; update checker
    Former-commit-id: 844fdf28a941534db1376f7b017f1cf1b43ad737 [formerly 9cab289bf8b49728ab341e7ead478344b8994081]
    Former-commit-id: 6d00824262756fea66c9ef54b9b9db35d5b32a3a

---
+++ ExViewer/VersionChecker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Web.Http;
+using Windows.Web.Http.Filters;
+
+namespace ExViewer
+{
+    static class VersionChecker
+    {
+        public static IAsyncOperation<bool> CheckAsync()
+        {
+            return AsyncInfo.Run(async token =>
+            {
+                var currentVersion = Package.Current.Id.Version;
+                try
+                {
+                    using(var filter = new HttpBaseProtocolFilter())
+                    {
+                        filter.CacheControl.ReadBehavior = HttpCacheReadBehavior.NoCache;
+                        filter.CacheControl.WriteBehavior = HttpCacheWriteBehavior.NoCache;
+                        using(var client = new HttpClient(filter))
+                        {
+                            var r = await client.GetAsync(new Uri("https://github.com/OpportunityLiu/ExViewer/releases/latest"));
+                            var version = new string(r.RequestMessage.RequestUri.Segments.Last().Select(c => char.IsDigit(c) ? c : ' ').ToArray());
+                            var subver = version.Split(default(char[]),  StringSplitOptions.RemoveEmptyEntries).Select(s => ushort.Parse(s)).ToArray();
+                            if(subver.Length != 3)
+                                return false;
+                            if(subver[0] < currentVersion.Major)
+                                return true;
+                            if(subver[1] < currentVersion.Minor)
+                                return true;
+                            if(subver[2] < currentVersion.Build)
+                                return true;
+                            return false;
+                        }
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            });
+        }
+    }
+}
---
+++ ExViewer/Views/UpdateDialog.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using ExViewer.Controls;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“内容对话框”项模板
+
+namespace ExViewer.Views
+{
+    public sealed partial class UpdateDialog : MyContentDialog
+    {
+        public UpdateDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+    }
+}
