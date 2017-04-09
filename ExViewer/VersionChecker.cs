f81bc911853268a86442264c782eef769785fbe3
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-04-09 18:22:32 +0800

    update nuget packages

--- ExViewer/VersionChecker.cs
+++ ExViewer/VersionChecker.cs
@@ -13,37 +13,39 @@ namespace ExViewer
 {
     static class VersionChecker
     {
-        public static IAsyncOperation<bool> CheckAsync()
+        public static Uri ReleaseUri { get; } = new Uri("https://github.com/OpportunityLiu/ExViewer/releases/latest");
+
+        public static IAsyncOperation<PackageVersion?> CheckAsync()
         {
-            return AsyncInfo.Run(async token =>
+            return AsyncInfo.Run<PackageVersion?>(async token =>
             {
                 var currentVersion = Package.Current.Id.Version;
-                try
+                using(var filter = new HttpBaseProtocolFilter())
                 {
-                    using(var filter = new HttpBaseProtocolFilter())
+                    filter.CacheControl.ReadBehavior = HttpCacheReadBehavior.NoCache;
+                    filter.CacheControl.WriteBehavior = HttpCacheWriteBehavior.NoCache;
+                    using(var client = new HttpClient(filter))
                     {
-                        filter.CacheControl.ReadBehavior = HttpCacheReadBehavior.NoCache;
-                        filter.CacheControl.WriteBehavior = HttpCacheWriteBehavior.NoCache;
-                        using(var client = new HttpClient(filter))
+                        var r = await client.GetAsync(ReleaseUri);
+                        var version = new string(r.RequestMessage.RequestUri.Segments.Last().Select(c => char.IsDigit(c) ? c : ' ').ToArray());
+                        var subver = version.Split(default(char[]), StringSplitOptions.RemoveEmptyEntries).Select(s => ushort.Parse(s)).ToArray();
+                        if(subver.Length != 3)
+                            return null;
+                        var newVersion = new PackageVersion
                         {
-                            var r = await client.GetAsync(new Uri("https://github.com/OpportunityLiu/ExViewer/releases/latest"));
-                            var version = new string(r.RequestMessage.RequestUri.Segments.Last().Select(c => char.IsDigit(c) ? c : ' ').ToArray());
-                            var subver = version.Split(default(char[]),  StringSplitOptions.RemoveEmptyEntries).Select(s => ushort.Parse(s)).ToArray();
-                            if(subver.Length != 3)
-                                return false;
-                            if(subver[0] < currentVersion.Major)
-                                return true;
-                            if(subver[1] < currentVersion.Minor)
-                                return true;
-                            if(subver[2] < currentVersion.Build)
-                                return true;
-                            return false;
-                        }
+                            Major = subver[0],
+                            Minor = subver[1],
+                            Build = subver[2],
+                            Revision = 0
+                        };
+                        if(newVersion.Major > currentVersion.Major)
+                            return newVersion;
+                        if(newVersion.Minor > currentVersion.Minor)
+                            return newVersion;
+                        if(newVersion.Build > currentVersion.Build)
+                            return newVersion;
+                        return null;
                     }
-                }
-                catch
-                {
-                    return false;
                 }
             });
         }
