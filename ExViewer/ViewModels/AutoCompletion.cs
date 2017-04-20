10630f7dd41f2b1145ce697273660bdacaaaccf0
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-04-20 16:12:36 +0800

    better input prediction
    Former-commit-id: d35e3f8d13b870be76cd3bcb5f3eea9e905de7da [formerly 443c867ecacc2dd4e7edc46ad69eeb1f9055780c]
    Former-commit-id: 21d2371ff570fb077b147107ea49b11565c5058d

---
+++ ExViewer/ViewModels/AutoCompletion.cs
@@ -0,0 +1,108 @@
+﻿using ExClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExViewer.ViewModels
+{
+    public class AutoCompletion
+    {
+        private AutoCompletion(string content)
+        {
+            this.Content = content;
+        }
+
+        public override string ToString()
+        {
+            return this.Content;
+        }
+
+        public string Content { get; private set; }
+
+        internal static IEnumerable<AutoCompletion> GetCompletions(string input)
+        {
+            if(string.IsNullOrWhiteSpace(input))
+                return getCompletionsWithEmptyInput();
+            var quoteCount = input.Count(c => c == '\"');
+            if(quoteCount % 2 == 0)
+                return getCompletionsWithQuoteFinished(input);
+            else
+                return getCompletionsWithQuoteUnfinished(input);
+        }
+
+        static AutoCompletion()
+        {
+            var ns = Enum.GetNames(typeof(Namespace)).ToList();
+            ns.Remove(Namespace.Misc.ToString());
+            ns.Remove(Namespace.Unknown.ToString());
+            for(var i = 0; i < ns.Count; i++)
+            {
+                ns[i] = ns[i].ToLowerInvariant();
+            }
+            ns.Add("uploader");
+            namedNamespaces = ns.AsReadOnly();
+        }
+
+        private static readonly IReadOnlyList<string> namedNamespaces;
+
+        private static IEnumerable<AutoCompletion> getCompletionsWithEmptyInput()
+        {
+            yield break;
+        }
+
+        private static IEnumerable<AutoCompletion> getCompletionsWithQuoteUnfinished(string input)
+        {
+            var lastChar = input[input.Length - 1];
+            switch(lastChar)
+            {
+            case ' ':
+            case ':':
+            case '"':
+                yield break;
+            case '$':
+                yield return new AutoCompletion($"{input}\"");
+                yield break;
+            case '-':
+            default:
+                yield return new AutoCompletion($"{input}\"");
+                yield return new AutoCompletion($"{input}$\"");
+                yield break;
+            }
+        }
+
+        private static IEnumerable<AutoCompletion> getCompletionsWithQuoteFinished(string input)
+        {
+            var lastChar = input[input.Length - 1];
+            switch(lastChar)
+            {
+            case ' ':
+            case '-':
+                // Too many results
+                //foreach(var item in namedNamespaces)
+                //{
+                //    yield return new AutoCompletion($"{input}{item}:");
+                //}
+                yield break;
+            case ':':
+                yield return new AutoCompletion($"{input}\"");
+                yield break;
+            case '"':
+            case '$':
+                yield break;
+            default:
+                var index = input.LastIndexOf(' ') + 1;
+                var lastTerm = input.Substring(index);
+                if(lastTerm.Length > 0 && lastTerm.All(c => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')))
+                {
+                    var beforeLastTerm = input.Substring(0, input.Length - lastTerm.Length);
+                    foreach(var item in namedNamespaces)
+                    {
+                        if(item.StartsWith(lastTerm, StringComparison.OrdinalIgnoreCase))
+                            yield return new AutoCompletion($"{beforeLastTerm}{item}:");
+                    }
+                }
+                yield break;
+            }
+        }
+    }
+}
