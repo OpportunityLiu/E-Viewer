6d5b9211a4bf8d9148f7099d1045adee673af1ad
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-05-01 11:16:39 +0800

    new version
    Former-commit-id: e7ddc57735a4b9ac10a3932054ba0b22495467c8 [formerly 4d80916ed6facdbe2e3e2e30b14a33ed32d2f60f]
    Former-commit-id: be88721ec626b09e0bf8e151d5c7c8464a2bcd0d

--- ExViewer/Controls/AboutControl.xaml.cs
+++ ExViewer/Controls/AboutControl.xaml.cs
@@ -14,8 +14,9 @@ namespace ExViewer.Controls
             var version = Package.Current.Id.Version;
             this.tb_AppVersion.Text = string.Format(CultureInfo.CurrentCulture, "{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
             this.tb_AppName.Text = Package.Current.DisplayName;
-#if !RELEASE
+#if DEBUG
             FindName(nameof(this.tb_DebugTag));
+            this.tb_DebugTag.Visibility = Visibility.Visible;
 #endif
             this.tb_AppAuthor.Text = Package.Current.PublisherDisplayName;
             this.tb_AppDescription.Text = Package.Current.Description;
