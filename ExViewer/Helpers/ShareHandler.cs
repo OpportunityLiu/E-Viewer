e41bcaa595c2e46f558ee6db004e3eb86d7e3df4
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-05-04 19:50:32 +0800

    new sharing experence
    Former-commit-id: 34607fac2273b19a080eb0f6d4159795adf77a87 [formerly 15867ad10a04d495dceefd9e7f56d28b146cf3fc]
    Former-commit-id: fefaac41046d6444eefacbb3bc600bde03c04c75

--- ExViewer/Helpers/ShareHandler.cs
+++ ExViewer/Helpers/ShareHandler.cs
@@ -1,7 +1,9 @@
-﻿using Opportunity.MvvmUniverse.Helpers;
+﻿using ExViewer.Views;
+using Opportunity.MvvmUniverse.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.ApplicationModel;
@@ -30,7 +32,7 @@ namespace ExViewer.Helpers
         {
             private static ShareProvider openLinkProvider
                 = new ShareProvider(Strings.Resources.Sharing.OpenInBrowser
-                    , RandomAccessStreamReference.CreateFromUri(new Uri(@"ms-appx:///Images/MicrosoftEdgeSquare44x44.png"))
+                    , RandomAccessStreamReference.CreateFromUri(new Uri(@"ms-appx:///Images/MicrosoftEdge.png"))
                     , (Color)Windows.UI.Xaml.Application.Current.Resources["SystemAccentColor"]
                     , async operation =>
                     {
@@ -39,8 +41,84 @@ namespace ExViewer.Helpers
                         DispatcherHelper.BeginInvokeOnUIThread(async () =>
                         {
                             await Launcher.LaunchUriAsync(uri, new LauncherOptions { IgnoreAppUriHandlers = true });
+                            operation.ReportCompleted();
                         });
                     });
+            private static ShareProvider copyProvider
+                = new ShareProvider(Strings.Resources.Sharing.CopyToClipboard
+                    , RandomAccessStreamReference.CreateFromUri(new Uri(@"ms-appx:///Images/CopyToClipboard.png"))
+                    , (Color)Windows.UI.Xaml.Application.Current.Resources["SystemAccentColor"]
+                    , async operation =>
+                    {
+                        var data = operation.Data;
+                        var pac = await DataProviderProxy.CreateAsync(data);
+                        await Task.Delay(500);
+                        DispatcherHelper.BeginInvokeOnUIThread(() =>
+                        {
+                            Clipboard.SetContent(pac.View);
+                            RootControl.RootController.SendToast(Strings.Resources.Sharing.CopyedToClipboard, null);
+                            operation.ReportCompleted();
+                        });
+                    });
+
+            private class DataProviderProxy
+            {
+                private DataPackageView viewToProxy;
+
+                public static IAsyncOperation<DataProviderProxy> CreateAsync(DataPackageView viewToProxy)
+                {
+                    if (viewToProxy == null)
+                        throw new ArgumentNullException(nameof(viewToProxy));
+                    return AsyncInfo.Run(async token =>
+                    {
+                        var proxy = new DataProviderProxy(viewToProxy);
+                        var view = proxy.View;
+                        foreach (var item in await viewToProxy.GetResourceMapAsync())
+                        {
+                            view.ResourceMap.Add(item.Key, item.Value);
+                        }
+                        foreach (var item in viewToProxy.Properties)
+                        {
+                            view.Properties.Add(item.Key, item.Value);
+                        }
+                        foreach (var formatId in viewToProxy.AvailableFormats)
+                        {
+                            if (!view.Properties.FileTypes.Contains(formatId))
+                                view.Properties.FileTypes.Add(formatId);
+                            view.SetDataProvider(formatId, proxy.dataProvider);
+                        }
+                        return proxy;
+                    });
+                }
+
+                private DataProviderProxy(DataPackageView viewToProxy)
+                {
+                    this.viewToProxy = viewToProxy;
+                    this.View = new DataPackage
+                    {
+                        RequestedOperation = viewToProxy.RequestedOperation
+                    };
+                }
+
+                private async void dataProvider(DataProviderRequest request)
+                {
+                    var d = request.GetDeferral();
+                    try
+                    {
+                        var result = await this.viewToProxy.GetDataAsync(request.FormatId);
+                        request.SetData(result);
+                    }
+                    finally
+                    {
+                        d.Complete();
+                    }
+                }
+
+                public DataPackage View
+                {
+                    get;
+                }
+            }
 
             public ShareHandlerStorage(TypedEventHandler<DataTransferManager, DataRequestedEventArgs> handler)
             {
@@ -53,6 +131,7 @@ namespace ExViewer.Helpers
             private void T_ShareProvidersRequested(DataTransferManager sender, ShareProvidersRequestedEventArgs args)
             {
                 sender.ShareProvidersRequested -= this.T_ShareProvidersRequested;
+                args.Providers.Add(copyProvider);
                 if (args.Data.Contains(StandardDataFormats.WebLink))
                 {
                     args.Providers.Add(openLinkProvider);
