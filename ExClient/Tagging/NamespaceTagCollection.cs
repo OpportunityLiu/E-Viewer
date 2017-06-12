ad619ffb56b1a208ef495b4aa100fbd2b86f1140
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-06-12 20:31:49 +0800

    tag notify

--- ExClient/Tagging/NamespaceTagCollection.cs
+++ ExClient/Tagging/NamespaceTagCollection.cs
@@ -4,67 +4,91 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using IReadOnlyList = System.Collections.Generic.IReadOnlyList<ExClient.Tagging.Tag>;
 using System;
+using System.Collections.Specialized;
 
 namespace ExClient.Tagging
 {
-    public sealed class TagCollection
+    [DebuggerDisplay(@"\{Namespace = {Namespace} Count = {Count}\}")]
+    public sealed class NamespaceTagCollection : ObservableCollectionBase, IReadOnlyList, IList
     {
-        [DebuggerDisplay(@"\{Namespace = {Namespace} Count = {Count}\}")]
-        public sealed class NamespaceTagCollection : ObservableCollectionBase, IReadOnlyList, IList
+        internal NamespaceTagCollection(TagCollection owner, int index)
         {
-
-            internal NamespaceTagCollection(TagCollection owner, int index)
-            {
-                this.Owner = owner;
-                this.index = index;
-            }
-
-            internal void RaiseChangeAt(int index)
-            {
-                this.RaiseCollectionReplace(this[index], this[index], index);
-            }
-
-            private readonly int index;
-
-            public TagCollection Owner { get; }
-
-            public Namespace Namespace => this.Owner.keys[this.index];
-
-            public int Count => this.Owner.offset[this.index + 1] - this.Owner.offset[this.index];
-
-            public bool IsFixedSize => true;
-
-            public bool IsReadOnly => true;
-
-            public bool IsSynchronized => false;
-
-            public object SyncRoot => ((IList)this.Owner).SyncRoot;
-
-            object IList.this[int index] { get => this.data[index]; set => throw new InvalidOperationException(); }
-
-            public Tag this[int index] => this.data[index];
-
-            public RangedCollectionView<Tag>.RangedCollectionViewEnumerator GetEnumerator() => this.data.GetEnumerator();
-
-            IEnumerator<Tag> IEnumerable<Tag>.GetEnumerator() => GetEnumerator();
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-            int IList.Add(object value) => throw new InvalidOperationException();
-
-            void IList.Clear() => throw new InvalidOperationException();
-
-            bool IList.Contains(object value) => ((IList)this.data).Contains(value);
-
-            int IList.IndexOf(object value) => ((IList)this.data).IndexOf(value);
-
-            void IList.Insert(int index, object value) => throw new InvalidOperationException();
-
-            void IList.Remove(object value) => throw new InvalidOperationException();
-
-            void IList.RemoveAt(int index) => throw new InvalidOperationException();
-
-            public void CopyTo(Array array, int index) => ((IList)this.data).CopyTo(array, index);
+            this.Owner = owner;
+            this.version = owner.version;
+            this.index = index;
+            owner.itemStateChanged += this.Owner_itemStateChanged; ;
         }
+
+        private void Owner_itemStateChanged(int index)
+        {
+            if (this.version != this.Owner.version)
+            {
+                dispose();
+                return;
+            }
+            var offset = this.Owner.offset[this.index];
+            if (offset <= index && index < this.Owner.offset[this.index + 1])
+            {
+                var item = this.Owner.data[index];
+                RaiseCollectionReplace(item, item, index - offset);
+            }
+        }
+
+        private void dispose()
+        {
+            if (this.Owner == null)
+                return;
+            this.Owner.itemStateChanged -= this.Owner_itemStateChanged;
+            this.Owner = null;
+        }
+
+        private readonly int index;
+
+        private readonly int version;
+
+        public TagCollection Owner { get; private set; }
+
+        public Namespace Namespace => this.Owner.keys[this.index];
+
+        public int Count => this.Owner.offset[this.index + 1] - this.Owner.offset[this.index];
+
+        public bool IsFixedSize => true;
+
+        public bool IsReadOnly => true;
+
+        public bool IsSynchronized => false;
+
+        public object SyncRoot => ((IList)this.Owner).SyncRoot;
+
+        object IList.this[int index] { get => this[index]; set => throw new InvalidOperationException(); }
+
+        public Tag this[int index] => this.Owner.data[this.Owner.offset[this.index] + index];
+
+        public IEnumerator<Tag> GetEnumerator()
+        {
+            var end = this.Owner.offset[this.index + 1];
+            for (var i = this.Owner.offset[this.index]; i < end; i++)
+            {
+                yield return this.Owner.data[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        int IList.Add(object value) => throw new InvalidOperationException();
+
+        void IList.Clear() => throw new InvalidOperationException();
+
+        bool IList.Contains(object value) => throw new InvalidOperationException();
+
+        int IList.IndexOf(object value) => throw new InvalidOperationException();
+
+        void IList.Insert(int index, object value) => throw new InvalidOperationException();
+
+        void IList.Remove(object value) => throw new InvalidOperationException();
+
+        void IList.RemoveAt(int index) => throw new InvalidOperationException();
+
+        public void CopyTo(Array array, int index) => throw new InvalidOperationException();
     }
 }
