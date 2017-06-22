133d0b7fd273ad90c6e3c372df7bf9d9ff2a1c01
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-06-22 12:05:01 +0800

    show fav info for popular
    Former-commit-id: f3b5e8166c2641584a80c9f28fb448c096867dea [formerly 91da0a9b2b9d14c3b145618ddbb05f90e3f0d600]
    Former-commit-id: d5b6c8cc2c53a260d33cebada59b13511ca1d49a

--- ExClient/Search/PopularCollection.cs
+++ ExClient/Search/PopularCollection.cs
@@ -27,18 +27,34 @@ namespace ExClient.Search
             this.PageCount = 1;
         }
 
+        private void handleAdditionalInfo(HtmlNode trNode, Gallery gallery)
+        {
+            var infoNode = trNode.ChildNodes[2].LastChild.FirstChild;
+            var favNode = infoNode.ChildNodes.FirstOrDefault(n => n.Id.StartsWith("favicon"));
+            gallery.FavoriteCategory = Client.Current.Favorites.GetCategory(favNode);
+        }
+
         protected override IAsyncOperation<IList<Gallery>> LoadPageAsync(int pageIndex)
         {
             return AsyncInfo.Run(async token =>
             {
                 var doc = await Client.Current.HttpClient.GetDocumentAsync(UriProvider.Eh.RootUri);
                 var pp = doc.GetElementbyId("pp");
-                var ginfo = (from div in pp.Elements("div")
+                var nodes = (from div in pp.Elements("div")
                              where div.GetAttributeValue("class", "") == "id1"
-                             let link = div.Descendants("a").First().GetAttributeValue("href", "")
-                             select GalleryInfo.Parse(new Uri(link))).ToList();
+                             select div).ToList();
+                var ginfo = nodes.Select(n =>
+                {
+                    var link = n.Descendants("a").First().GetAttributeValue("href", "");
+                    return GalleryInfo.Parse(new Uri(link));
+                }).ToList();
                 this.RecordCount = ginfo.Count;
-                return await Gallery.FetchGalleriesAsync(ginfo);
+                var galleries = await Gallery.FetchGalleriesAsync(ginfo);
+                for (var i = 0; i < ginfo.Count; i++)
+                {
+                    handleAdditionalInfo(nodes[i], galleries[i]);
+                }
+                return galleries;
             });
         }
     }
