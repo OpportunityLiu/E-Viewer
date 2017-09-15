e0c18bbded6060bd1abb8989d8a53a3915ab2426
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-09-15 18:00:37 +0800

    new tag translate db
    Former-commit-id: 985cc82c45e83184720621c5c42a5582260a09ad [formerly cc62412ee2ba69480dda97fe8cd0a2edb02f5faf]
    Former-commit-id: 03647200814b61c51e674b6f3e8d5c4cd7fba942

---
+++ EhTagTranslatorClient/Migrations/20170915083036_ExternalLinks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EhTagTranslatorClient.Migrations
+{
+    public partial class ExternalLinks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalLinksRaw",
+                table: "Table",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalLinksRaw",
+                table: "Table");
+        }
+    }
+}
--- EhTagTranslatorClient/Record.cs
+++ EhTagTranslatorClient/Record.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using Windows.Storage.Streams;
@@ -13,24 +14,27 @@ namespace EhTagTranslatorClient
     public class Record
     {
         private static Regex lineRegex = new Regex(
-            $@"^\s*(?<!\\)\|?\s*
+            $@"
+            ^\s*(?<!\\)\|?\s*
             (?<{nameof(Original)}>.*?)
 		    \s*(?<!\\)\|\s*
 		    (?<{nameof(Translated)}>.*?)
 		    \s*(?<!\\)\|\s*
 		    (?<{nameof(Introduction)}>.*?)
+		    \s*(?<!\\)\|\s*
+		    (?<{nameof(ExternalLinks)}>.*?)
 		    \s*(?<!\\)\|?\s*$", RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.IgnorePatternWhitespace);
 
         public static IEnumerable<Record> Analyze(IInputStream stream, Namespace @namespace)
         {
             var skipcount = 0;
             var reader = new StreamReader(stream.AsStreamForRead());
-            while(!reader.EndOfStream)
+            while (!reader.EndOfStream)
             {
                 var r = AnalyzeLine(reader.ReadLine(), @namespace);
-                if(r != null)
+                if (r != null)
                 {
-                    if(skipcount >= 2)
+                    if (skipcount >= 2)
                         yield return r;
                     else
                         skipcount++;
@@ -41,19 +45,20 @@ namespace EhTagTranslatorClient
         internal static Record AnalyzeLine(string line, Namespace @namespace)
         {
             var match = lineRegex.Match(line);
-            if(!match.Success)
+            if (!match.Success)
                 return null;
             var ori = match.Groups[nameof(Original)].Value;
-            if(string.IsNullOrEmpty(ori))
+            if (string.IsNullOrEmpty(ori))
                 return null;
             var tra = match.Groups[nameof(Translated)].Value;
             var intro = match.Groups[nameof(Introduction)].Value;
-            return new Record(@namespace, unescape(ori), unescape(tra), unescape(intro));
+            var link = match.Groups[nameof(ExternalLinks)].Value;
+            return new Record(@namespace, unescape(ori), unescape(tra), unescape(intro), unescape(link));
         }
 
         private static string unescape(string value)
         {
-            if(value.Contains("<br>") || value.Contains(@"\"))
+            if (value.Contains("<br>") || value.Contains(@"\"))
             {
                 var sb = new StringBuilder(value);
                 sb.Replace(@"\|", @"|");
@@ -68,15 +73,15 @@ namespace EhTagTranslatorClient
 
         internal Record() { }
 
-        internal static Record Combine(Record r1,Record r2)
+        internal static Record Combine(Record r1, Record r2)
         {
-            if(r1.Original != r2.Original)
+            if (r1.Original != r2.Original)
                 throw new InvalidOperationException();
-            if(r1.Namespace != r2.Namespace)
+            if (r1.Namespace != r2.Namespace)
                 throw new InvalidOperationException();
             string original, translated, intro;
             original = r1.Original;
-            if(r1.TranslatedRaw==r2.TranslatedRaw)
+            if (r1.TranslatedRaw == r2.TranslatedRaw)
             {
                 translated = r1.TranslatedRaw;
             }
@@ -84,61 +89,43 @@ namespace EhTagTranslatorClient
             {
                 translated = $@"{r1.TranslatedRaw} | {r2.TranslatedRaw}";
             }
-            if(string.IsNullOrWhiteSpace(r1.IntroductionRaw))
+            if (string.IsNullOrWhiteSpace(r1.IntroductionRaw))
                 intro = r2.IntroductionRaw;
-            else if(string.IsNullOrWhiteSpace(r2.IntroductionRaw))
+            else if (string.IsNullOrWhiteSpace(r2.IntroductionRaw))
                 intro = r1.IntroductionRaw;
             else
                 intro = $"{r1.IntroductionRaw}<hr>{r2.IntroductionRaw}";
-            return new Record(r1.Namespace, r1.Original, translated, intro);
+            return new Record(r1.Namespace, r1.Original, translated, intro, $"{r1.ExternalLinksRaw} {r2.ExternalLinksRaw}");
         }
 
-        private Record(Namespace @namespace, string original, string translated, string introduction)
+        private Record(Namespace @namespace, string original, string translated, string introduction, string externalLinks)
         {
             this.Namespace = @namespace;
             this.Original = original;
             this.TranslatedRaw = translated;
             this.IntroductionRaw = introduction;
             this.TranslatedStr = this.Translated.Text;
-        }
-        
-        public string Original
-        {
-            get;
-            internal set;
+            this.ExternalLinksRaw = externalLinks;
         }
 
-        public string TranslatedRaw
-        {
-            get;
-            internal set;
-        }
+        public string Original { get; internal set; }
 
-        public string TranslatedStr
-        {
-            get;
-            internal set;
-        }
+        public string TranslatedRaw { get; internal set; }
+
+        public string TranslatedStr { get; internal set; }
 
         public MarkdownText Translated => new MarkdownText(TranslatedRaw);
 
-        public string IntroductionRaw
-        {
-            get;
-            internal set;
-        }
-        
-        public MarkdownText Introduction => new MarkdownText(IntroductionRaw);
-        
-        public Namespace Namespace
-        {
-            get;
-            internal set;
-        }
+        public string IntroductionRaw { get; internal set; }
 
-        public Tag AsTag()
-        {
-            return new Tag(this.Namespace, this.Original);
-        }
+        public MarkdownText Introduction => new MarkdownText(IntroductionRaw);
+
+        public string ExternalLinksRaw { get; internal set; }
+
+        public MarkdownText ExternalLinks => new MarkdownText(ExternalLinksRaw);
+
+        public Namespace Namespace { get; internal set; }
+
+        public Tag AsTag() => new Tag(this.Namespace, this.Original);
     }
 }
