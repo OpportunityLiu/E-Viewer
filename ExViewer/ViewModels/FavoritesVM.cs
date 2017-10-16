551e11c47adddfc160e3ff21fbdcb87b0edbfd70
OpportunityLiu <liuzhengyuan13@outlook.com>
2017-10-16 23:53:07 +0800

    bug fix for fav search
    Former-commit-id: e28c69abdded8fd8ba8b47cffba011a3952d38b2 [formerly 8b00adfaaeb83b7c84a38dfdd101842250752e52]
    Former-commit-id: 4841dfdc4184757fd9e50213e174a7a76d9f6890

--- ExViewer/ViewModels/FavoritesVM.cs
+++ ExViewer/ViewModels/FavoritesVM.cs
@@ -102,10 +102,10 @@ namespace ExViewer.ViewModels
             {
                 var q = JsonConvert.DeserializeObject<SearchResultData>(parameter);
                 this.keyword = q.Keyword;
-                if (q.CategoryIndex > 0)
-                    this.category = Client.Current.Favorites[q.CategoryIndex];
-                else
+                if (q.CategoryIndex < 0 || q.CategoryIndex >= Client.Current.Favorites.Count)
                     this.category = FavoriteCategory.All;
+                else
+                    this.category = Client.Current.Favorites[q.CategoryIndex];
             }
             this.SearchResult = Client.Current.Favorites.Search(this.keyword, this.category);
         }
