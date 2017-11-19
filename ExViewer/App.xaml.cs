0519711f4866718958b8decd6a5ae73d727437f3
OpportunityLiu <Opportunity@live.in>
2017-11-19 12:04:12 +0800

    new ver
    Former-commit-id: 767ed92287fbe8c9a6f25b06124d2be5b4c91ada [formerly 1632eea9e54d01df07f7c6924f40f5bbb12ef03d]
    Former-commit-id: a9c9a5faa4d687df71fb40847821a0c6ecd7298e

--- ExViewer/App.xaml.cs
+++ ExViewer/App.xaml.cs
@@ -6,12 +6,12 @@ using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
-using Microsoft.Azure.Mobile;
 using System.Collections.Generic;
-using Microsoft.Azure.Mobile.Analytics;
-using Microsoft.Azure.Mobile.Crashes;
+using Microsoft.AppCenter.Analytics;
 #if !DEBUG
 using Microsoft.HockeyApp;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Crashes;
 #endif
 
 namespace ExViewer
@@ -77,11 +77,11 @@ namespace ExViewer
         private async void lanunchCore(IActivatedEventArgs e, bool prelaunchActivated)
         {
 #if !DEBUG
-            if (!MobileCenter.Configured)
+            if (!AppCenter.Configured)
             {
                 var region = new Windows.Globalization.GeographicRegion();
-                MobileCenter.SetCountryCode(region.CodeTwoLetter);
-                MobileCenter.Start("4b9c5e4f-ebf5-46ed-9ee8-72e5de8e0236", typeof(Analytics), typeof(Crashes));
+                AppCenter.SetCountryCode(region.CodeTwoLetter);
+                AppCenter.Start("4b9c5e4f-ebf5-46ed-9ee8-72e5de8e0236", typeof(Analytics), typeof(Crashes));
             }
 #endif
             if (!Opportunity.MvvmUniverse.DispatcherHelper.Initialized)
