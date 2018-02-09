ae5498266dbd186432e71c064627d7fb0c3809b1
OpportunityLiu <Opportunity@live.in>
2018-02-09 16:17:04 +0800

    VM

--- ExViewer/Controls/CommentViewer.xaml.cs
+++ ExViewer/Controls/CommentViewer.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using ExClient;
 using ExClient.Galleries.Commenting;
 using ExViewer.Views;
+using Opportunity.MvvmUniverse;
+using Opportunity.MvvmUniverse.Commands;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -23,12 +25,99 @@ namespace ExViewer.Controls
 {
     public sealed partial class CommentViewer : UserControl
     {
+        private class CommentVM : ViewModelBase
+        {
+            public CommentVM()
+            {
+                this.Translate.Tag = this;
+            }
+
+            private Comment comment;
+            public Comment Comment
+            {
+                get => this.comment;
+                set
+                {
+                    if (Set(ref this.comment, value))
+                    {
+                        TranslatedContent = null;
+                    }
+                }
+            }
+
+            public static bool CanVoteUp(CommentStatus status) => status == CommentStatus.Votable || status == CommentStatus.VotedDown;
+            public static bool CanVoteDown(CommentStatus status) => status == CommentStatus.Votable || status == CommentStatus.VotedUp;
+            public static bool CanVoteWithdraw(CommentStatus status) => status == CommentStatus.VotedUp || status == CommentStatus.VotedDown;
+
+            public Visibility IsVoteUpVisible(CommentStatus status) => CanVoteUp(status) ? Visibility.Visible : Visibility.Collapsed;
+            public Visibility IsVoteDownVisible(CommentStatus status) => CanVoteDown(status) ? Visibility.Visible : Visibility.Collapsed;
+            public Visibility IsVoteWithdrawVisible(CommentStatus status) => CanVoteWithdraw(status) ? Visibility.Visible : Visibility.Collapsed;
+
+            private HtmlAgilityPack.HtmlNode translated;
+            public HtmlAgilityPack.HtmlNode TranslatedContent
+            {
+                get => this.translated;
+                private set
+                {
+                    if (Set(ref this.translated, value))
+                        Translate.OnCanExecuteChanged();
+                }
+            }
+
+            public AsyncCommand<Comment> Translate { get; } = AsyncCommand.Create<Comment>(async (s, c) =>
+            {
+                var r = await c.TranslateAsync(Settings.SettingCollection.Current.CommentTranslationCode);
+                ((CommentVM)s.Tag).TranslatedContent = r;
+            }, (s, c) => c != null && ((CommentVM)s.Tag).translated == null);
+
+            public AsyncCommand<Comment> VoteUp { get; }
+                = AsyncCommand.Create<Comment>((s, c) => c.VoteAsync(ExClient.Api.VoteState.Up), (s, c) => c != null && CanVoteUp(c.Status));
+
+            public AsyncCommand<Comment> VoteDown { get; }
+                = AsyncCommand.Create<Comment>((s, c) => c.VoteAsync(ExClient.Api.VoteState.Down), (s, c) => c != null && CanVoteDown(c.Status));
+
+            public AsyncCommand<Comment> VoteWithdraw { get; }
+                = AsyncCommand.Create<Comment>((s, c) => c.VoteAsync(ExClient.Api.VoteState.Default), (s, c) => c != null && CanVoteWithdraw(c.Status));
+
+            private static EditCommentDialog editDialog;
+            public Command<Comment> Edit { get; } = Command.Create<Comment>(async (s, c) =>
+            {
+                var dialog = System.Threading.LazyInitializer.EnsureInitialized(ref editDialog);
+                dialog.EditableComment = c;
+                await dialog.ShowAsync();
+            }, (s, c) => c != null && c.CanEdit);
+
+            private static ReplyCommentDialog replyDialog;
+            public Command<Comment> Reply { get; } = Command.Create<Comment>(async (s, c) =>
+            {
+                var dialog = System.Threading.LazyInitializer.EnsureInitialized(ref replyDialog);
+                dialog.ReplyingComment = c;
+                await dialog.ShowAsync();
+            }, (s, c) => c != null && !c.CanEdit);
+        }
+
         public CommentViewer()
         {
             this.InitializeComponent();
+            this.VM.Translate.Executed += (s, e) =>
+            {
+                var ex = e.Exception;
+                if (ex != null)
+                    RootControl.RootController.SendToast(ex, typeof(GalleryPage));
+                else
+                    FindName(nameof(this.Translated));
+            };
+            this.VM.PropertyChanged += this.VM_PropertyChanged;
         }
 
-        private static EditCommentDialog editDialog = new EditCommentDialog();
+        private void VM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            var vm = (CommentVM)sender;
+            if (vm.Comment?.CanEdit ?? false)
+                FindName(nameof(this.AttentionHeader));
+        }
+
+        private readonly CommentVM VM = new CommentVM();
 
         public Comment Comment
         {
@@ -43,25 +132,8 @@ namespace ExViewer.Controls
         private static void CommentPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var sender = (CommentViewer)d;
-            sender.TranslatedContent = null;
-            if (e.NewValue is Comment c)
-            {
-                if (c.CanEdit)
-                {
-                    sender.FindName(nameof(AttentionHeader));
-                }
-            }
+            sender.VM.Comment = (Comment)e.NewValue;
         }
-
-        public HtmlAgilityPack.HtmlNode TranslatedContent
-        {
-            get => (HtmlAgilityPack.HtmlNode)GetValue(TranslatedContentProperty);
-            set => SetValue(TranslatedContentProperty, value);
-        }
-
-        // Using a DependencyProperty as the backing store for TranslatedContent.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty TranslatedContentProperty =
-            DependencyProperty.Register("TranslatedContent", typeof(HtmlAgilityPack.HtmlNode), typeof(CommentViewer), new PropertyMetadata(null));
 
         protected override void OnDisconnectVisualChildren()
         {
@@ -74,78 +146,7 @@ namespace ExViewer.Controls
             FindName(nameof(this.EngagementIndicator));
         }
 
-        private async void VoteUp_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                await this.Comment.VoteAsync(ExClient.Api.VoteState.Up);
-            }
-            catch (Exception ex)
-            {
-                RootControl.RootController.SendToast(ex, typeof(GalleryPage));
-            }
-        }
-
-        private async void VoteDown_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                await this.Comment.VoteAsync(ExClient.Api.VoteState.Down);
-            }
-            catch (Exception ex)
-            {
-                RootControl.RootController.SendToast(ex, typeof(GalleryPage));
-            }
-        }
-
-        private void VotePopUp_Opened(object sender, object e)
-        {
-            ((Popup)sender).Child.DescendantsAndSelf<Control>().First().Focus(FocusState.Programmatic);
-        }
-
-        private async void Edit_Click(object sender, RoutedEventArgs e)
-        {
-            editDialog.EditableComment = this.Comment;
-            await editDialog.ShowAsync();
-        }
-
-        private void WithdrawVote_Click(object sender, RoutedEventArgs e)
-        {
-            switch (this.Comment.Status)
-            {
-            case CommentStatus.VotedUp:
-                VoteUp_Click(sender, e);
-                break;
-            case CommentStatus.VotedDown:
-                VoteDown_Click(sender, e);
-                break;
-            }
-        }
-
-        private async void Reply_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = System.Threading.LazyInitializer.EnsureInitialized(ref replyDialog);
-            dialog.ReplyingComment = this.Comment;
-            await dialog.ShowAsync();
-        }
-
-        private static ReplyCommentDialog replyDialog;
-
-        private async void Translate_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                var translateTask = this.Comment.TranslateAsync(Settings.SettingCollection.Current.CommentTranslationCode);
-                FindName(nameof(this.Translated));
-                this.TranslatedContent = await translateTask;
-            }
-            catch (Exception ex)
-            {
-                RootControl.RootController.SendToast(ex, null);
-            }
-        }
-
-        private double toOpacity(HtmlAgilityPack.HtmlNode val)
+        private static double toOpacity(HtmlAgilityPack.HtmlNode val)
         {
             if (val == null)
                 return 1;
