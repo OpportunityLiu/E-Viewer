51eb30ffd8e995c578a959f98baf46439301139c
OpportunityLiu <opportunity@live.in>
2018-02-20 21:19:17 +0800

    fix script
    Former-commit-id: 029e75ae38ef989f7ee6fdeac744e8d7e98e0691 [formerly 5c6efffc4965bb0ad97c52df16355a623902c295]
    Former-commit-id: d3d6238e0578f66e88941a13c6953a4fce454a8a

--- appcenter-pre-build.ps1
+++ appcenter-pre-build.ps1
@@ -1,4 +1,4 @@
 $NugetPackage = nuget locals global-packages -list
-$NugetPackage = $NuetPackage.SubString(17)
-$ToolPath = Join-Path $NugetPackage 'opportunity.resourcegenerator\1.2.3\tools\Opportunity.ResourceGenerator.Generator.exe'
+$NugetPackage = $NugetPackage.SubString(17)
+$ToolPath = Join-Path $NugetPackage 'Opportunity.ResourceGenerator\1.2.3\tools\Opportunity.ResourceGenerator.Generator.exe'
 &$ToolPath $Env:APPCENTER_UWP_SOLUTION
