f46e8cc999a4b2cf3380d593ee4482838a398962
OpportunityLiu <opportunity@live.in>
2018-02-24 19:11:05 +0800

    fix scripy

--- appcenter-pre-build.ps1
+++ appcenter-pre-build.ps1
@@ -1,4 +1,5 @@
 $NugetPackage = nuget locals global-packages -list
 $NugetPackage = $NugetPackage.SubString(17)
-$ToolPath = Join-Path $NugetPackage 'Opportunity.ResourceGenerator\1.2.3\tools\Opportunity.ResourceGenerator.Generator.exe'
+$ToolVersions = (Get-ChildItem (Join-Path $NugetPackage 'Opportunity.ResourceGenerator') | Sort-Object Name -Descending)[0]
+$ToolPath = Join-Path $ToolVersions.FullName '/tools/Opportunity.ResourceGenerator.Generator.exe'
 &$ToolPath $Env:APPCENTER_UWP_SOLUTION
