5fb2e1d539ee4632219f6d55a505453d5d400f51
OpportunityLiu <opportunity@live.in>
2018-03-12 10:23:53 +0800

    .
    Former-commit-id: c59c7f2c7e4c9aa0920a137c61eb4a2cc36c3502 [formerly e8c9d20911f2e84b9c82eb930255bbbc2ad4a96a]
    Former-commit-id: c86d1147198523a888238953b292fd4739729f3a

--- appcenter-post-build.ps1
+++ appcenter-post-build.ps1
@@ -1,6 +1,6 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $Path = Join-Path $Env:APPCENTER_SOURCE_DIRECTORY "\ExViewer\AppPackages"
-$Files = Get-ChildItem -Path $Path -Include @('*.cer', '*.appxbundle', '*.appx') -Recurse
+$Files = Get-ChildItem -Path $Path -Include @('*.cer', '*.appxbundle') -Recurse
 $Version = $Files[0].Name.Split('_')[1]
 $Auth = [convert]::ToBase64String([system.text.encoding]::UTF8.GetBytes("${ENV:GITHUB_USER}:${ENV:GITHUB_PASS}"))
 $DefaultHeader = @{
