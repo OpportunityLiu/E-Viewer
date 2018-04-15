db1466932f65c948955cc3b70486ce8d53ed2c60
OpportunityLiu <opportunity@live.in>
2018-04-15 21:15:55 +0800

    use spceicialized panel for layout

--- ExViewer/Helpers/ApiInfo.cs
+++ ExViewer/Helpers/ApiInfo.cs
@@ -6,14 +6,8 @@ namespace ExViewer
     {
         public static bool RS3 { get; } = IsApiContractPresent("Windows.Foundation.UniversalApiContract", 5);
 
-        public static bool StatusBarSupported
-        {
-            get;
-        } = IsTypePresent("Windows.UI.ViewManagement.StatusBar");
+        public static bool StatusBarSupported { get; } = IsTypePresent("Windows.UI.ViewManagement.StatusBar");
 
-        public static bool ShareProviderSupported
-        {
-            get;
-        } = IsTypePresent("Windows.ApplicationModel.DataTransfer.ShareProvider");
+        public static bool ShareProviderSupported { get; } = IsTypePresent("Windows.ApplicationModel.DataTransfer.ShareProvider");
     }
 }
