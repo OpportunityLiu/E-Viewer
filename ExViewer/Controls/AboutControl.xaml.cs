ee6612a8e9ae72fb8e8c6bdd8b538c982fd84726
OpportunityLiu <opportunity@live.in>
2018-10-24 01:03:26 +0800

    fix focus

--- ExViewer/Controls/AboutControl.xaml.cs
+++ ExViewer/Controls/AboutControl.xaml.cs
@@ -9,6 +9,7 @@ using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Storage;
 using Windows.System;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
