ac2617b3b169d749690e9405cc8971b8329b670a
OpportunityLiu <opportunity@live.in>
2019-02-21 11:55:10 +0800

    support open lofi urls
    Former-commit-id: 192a4a122d14dfedfdeec95717d744c25985dab1 [formerly 128dae0678174fdeffb36b918867d5209b8a5baa]
    Former-commit-id: e82076711a336dea04314313c166b7b06a49692c

--- ExClient/Launch/UriLauncher.cs
+++ ExClient/Launch/UriLauncher.cs
@@ -18,28 +18,32 @@ namespace ExClient.Launch
             new GalleryImageHandler(),
         };
 
-        private static UriHandlerData previousData;
-        private static UriHandler previousHandler;
+        private static UriHandlerData _PreviousData;
+        private static UriHandler _PreviousHandler;
+
+        private static void _RewriteUri(ref Uri uri)
+        {
+            if (uri.Host == "lofi.e-hentai.org")
+                uri = new Uri("https://" + DomainProvider.Eh.RootUri.Host + uri.PathAndQuery + uri.Fragment);
+            if (uri.PathAndQuery.StartsWith("/lofi/"))
+                uri = new Uri("https://" + uri.Host + uri.PathAndQuery.Substring("/lofi".Length) + uri.Fragment);
+        }
 
         public static bool CanHandle(Uri uri)
         {
             if (uri is null)
-            {
                 return false;
-            }
-
+            _RewriteUri(ref uri);
             if (uri.Host != DomainProvider.Eh.RootUri.Host && uri.Host != DomainProvider.Ex.RootUri.Host)
-            {
                 return false;
-            }
 
             var data = new UriHandlerData(uri);
             foreach (var item in handlers)
             {
                 if (item.CanHandle(data))
                 {
-                    previousData = data;
-                    previousHandler = item;
+                    _PreviousData = data;
+                    _PreviousHandler = item;
                     return true;
                 }
             }
@@ -48,8 +52,11 @@ namespace ExClient.Launch
 
         public static IAsyncOperation<LaunchResult> HandleAsync(Uri uri)
         {
-            var data = previousData;
-            var h = previousHandler;
+            if (uri is null)
+                throw new ArgumentNullException(nameof(uri));
+            _RewriteUri(ref uri);
+            var data = _PreviousData;
+            var h = _PreviousHandler;
             if (data?.Uri == uri && h != null && h.CanHandle(data))
             {
                 return h.HandleAsync(data);
