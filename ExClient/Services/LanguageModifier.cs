39e7af3368426dc8413f2cd1e8639fb88e011150
OpportunityLiu <opportunity@live.in>
2019-03-21 10:46:17 +0800

    optimize language

--- ExClient/Services/LanguageModifier.cs
+++ ExClient/Services/LanguageModifier.cs
@@ -1,5 +1,21 @@
 ﻿namespace ExClient.Services
 {
+    public static class LanguageModifierExtension
+    {
+        public static string ToFriendlyNameString(this LanguageModifier that)
+        {
+            switch (that)
+            {
+            case LanguageModifier.Translated:
+                return LocalizedStrings.Language.Modifiers.Translated;
+            case LanguageModifier.Rewrite:
+                return LocalizedStrings.Language.Modifiers.Rewrite;
+            default:
+                return "";
+            }
+        }
+    }
+
     public enum LanguageModifier
     {
         None,
